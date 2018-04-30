5b163bdb1904d2c9403d45f8a88795cd3500f9d0
Charly Mannion <charlottemannion100@gmail.com>
2018-04-30 17:47:01 +0100

    first user story

--- app.rb
+++ app.rb
@@ -2,14 +2,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :name_form
   end
 
   post '/names' do
-    @player_1 = params[:Player_1]
-    @player_2 = params[:Player_2]
+    session[:player_1] = params[:Player_1]
+    session[:player_2] = params[:Player_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb :play
   end
 
